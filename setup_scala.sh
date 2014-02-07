e0948800d9873bc9fa38c3c08e9617d896a7f71d
folger <folger6@gmail.com>
2014-02-07 11:04:27 +0800

    support setup scala

---
+++ setup_scala.sh
@@ -0,0 +1,7 @@
+sudo ln -s /opt/scala-2.10.3/bin/scala /usr/local/bin
+sudo ln -s /opt/scala-2.10.3/bin/scalac /usr/local/bin
+sudo ln -s /opt/scala-2.10.3/bin/scalap /usr/local/bin
+sudo ln -s /opt/scala-2.10.3/bin/scaladoc /usr/local/bin
+sudo ln -s /opt/scala-2.10.3/bin/fsc /usr/local/bin
+sudo ln -s /opt/scala-2.10.3/bin/sbaz /usr/local/bin
+sudo ln -s /opt/scala-2.10.3/bin/sbaz-setup /usr/local/bin
