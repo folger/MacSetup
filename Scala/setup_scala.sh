34b5bc78dbabed349434b37e2dca89718bc7dd59
folger <folger6@gmail.com>
2014-02-09 09:58:33 +0800

    make Scala category

---
+++ Scala/setup_scala.sh
@@ -0,0 +1,7 @@
+ln -s /opt/scala-2.10.3/bin/scala /usr/local/bin
+ln -s /opt/scala-2.10.3/bin/scalac /usr/local/bin
+ln -s /opt/scala-2.10.3/bin/scalap /usr/local/bin
+ln -s /opt/scala-2.10.3/bin/scaladoc /usr/local/bin
+ln -s /opt/scala-2.10.3/bin/fsc /usr/local/bin
+ln -s /opt/scala-2.10.3/bin/sbaz /usr/local/bin
+ln -s /opt/scala-2.10.3/bin/sbaz-setup /usr/local/bin
--- setup_scala.sh
+++
@@ -1,7 +0,0 @@
-sudo ln -s /opt/scala-2.10.3/bin/scala /usr/local/bin
-sudo ln -s /opt/scala-2.10.3/bin/scalac /usr/local/bin
-sudo ln -s /opt/scala-2.10.3/bin/scalap /usr/local/bin
-sudo ln -s /opt/scala-2.10.3/bin/scaladoc /usr/local/bin
-sudo ln -s /opt/scala-2.10.3/bin/fsc /usr/local/bin
-sudo ln -s /opt/scala-2.10.3/bin/sbaz /usr/local/bin
-sudo ln -s /opt/scala-2.10.3/bin/sbaz-setup /usr/local/bin
